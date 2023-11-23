2f49f7924930ab8cd52cdde048c9c149acbf5e23
Alexandro Frisone Gomes <124726829+alexfrisone@users.noreply.github.com>
2023-11-23 19:00:31 -0300

    abrir a planilha

--- metrics/main_metricas_by_month.py
+++ metrics/main_metricas_by_month.py
@@ -1,6 +1,7 @@
 import metric_log
 import pandas as pd
 import services
+import subprocess
 
 MONTH = 10
 YEAR = 2023
@@ -9,6 +10,7 @@ def checkDeckMetrics():
     periodo, metrics, metrics_raw = services.getAllMetrics(MONTH,YEAR)
     print(metrics)
     metrics.to_excel('/Users/alexfrisoneyape/Development/EM/metricas/metrics.xlsx')
+    subprocess.run(['open', '-a', 'Microsoft Excel', '/Users/alexfrisoneyape/Development/EM/metricas/metrics.xlsx' ])
     
 if __name__ == "__main__":
     metric_log.config_metric_log()
