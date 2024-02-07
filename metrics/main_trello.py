612f25e2ce7715671e183599701b732175eb5491
Alexandro Frisone Gomes <124726829+alexfrisone@users.noreply.github.com>
2024-02-07 19:38:50 -0300

    removi o multithead porque carregava os dados errados

--- metrics/main_trello.py
+++ metrics/main_trello.py
@@ -1,19 +1,53 @@
 import metric_log
 import trello
 def execucao_diaria():
-    # list_id = "632b5ec4ee1230008bd34d7f"  #marketplace 
-    list_id = "620cbb3308424a3f218f092a" #promos
+    list_id = "632b5ec4ee1230008bd34d7f"  #marketplace 
+    # list_id = "620cbb3308424a3f218f092a" #promos
     # list_id = "61ae4a1ca02160403e4daeda" #devops
     from_date = '2023-12-01'  # Formato aaaa-mm-dd
     to_date = '2024-01-31'    # Formato aaaa-mm-dd
-    # board_id = '632b5ec4ee1230008bd34d6d' #marketplace
-    board_id = '620c10ba5cfd7d4ce889d72a' #promos
+    board_id = '632b5ec4ee1230008bd34d6d' #marketplace
+    # board_id = '620c10ba5cfd7d4ce889d72a' #promos
     # board_id = '61ae4a1ca02160403e4daece' #dev ops solicitudes
-    filename = 'detailed_interactions_cards_done_list_promos.xlsx'
-
+    # filename = 'detailed_interactions_cards_done_list_'
+    filename = 'detailed_interactions_cards_done_list_mktplace.xlsx'
+    # teams = [
+    #             {
+    #             "name": "marketplace",
+    #             "list_id": "632b5ec4ee1230008bd34d7f",
+    #             "board_id": "632b5ec4ee1230008bd34d6d",
+    #             },
+    #             {
+    #             "name": "promos",
+    #             "list_id": "620cbb3308424a3f218f092a",
+    #             "board_id": "620c10ba5cfd7d4ce889d72a",
+    #             }
+    #         ]
+    # for team in teams:
+    #     print(f"Getting {team['name']} data……")
+    #     trello.generateMetricsTrello(team['board_id'],team['list_id'], f"{filename}{team['name']}.xlsx",from_date, to_date)
     trello.generateMetricsTrello(board_id,list_id, filename,from_date, to_date)
 
 
 if __name__ == "__main__":
     metric_log.config_metric_log()
-    execucao_diaria()
+    # execucao_diaria()
+    from_date = '2023-12-01'  # Formato aaaa-mm-dd
+    to_date = '2024-01-31'    # Formato aaaa-mm-dd
+    filename = 'detailed_interactions_cards_done_list_'
+    teams = [
+                # {
+                # "name": "marketplace",
+                # "list_id": "632b5ec4ee1230008bd34d7f",
+                # "board_id": "632b5ec4ee1230008bd34d6d",
+                # }
+                # ,
+                {
+                "name": "promos",
+                "list_id": "620cbb3308424a3f218f092a",
+                "board_id": "620c10ba5cfd7d4ce889d72a",
+                }
+            ]
+    for team in teams:
+        print(f"Getting {team['name']} data……")
+        trello.generateMetricsTrello(team['board_id'],team['list_id'], f"{filename}{team['name']}.xlsx",from_date, to_date)
--- metrics/trello.py
+++ metrics/trello.py
@@ -60,6 +60,7 @@ def process_card(card, api_key, token, from_date, to_date):
     card_name = card['name']
     card_url = card['shortUrl']
     actions = get_actions_for_card(card_id, api_key, token, from_date, to_date)
+    # print(f"Total de actions: {len(actions)}")
     card_member_info = []
     for action in actions:
         if 'member' in action['data']:
@@ -88,60 +89,60 @@ def get_card_in_done(list_id):
     cards = response.json() if response.status_code == 200 else []
     return cards
 
+def filter_cards_by_column(cards, column_id, api_key, token, from_date_str, to_date_str):
+    # Convertendo as strings de data para objetos datetime
+    from_date = datetime.strptime(from_date_str, '%Y-%m-%d')
+    to_date = datetime.strptime(to_date_str, '%Y-%m-%d')
+
+    filtered_cards = []
+
+    for card in tqdm(cards, desc='Filtering cards'):
+        card_movements = get_card_movements(card['id'], api_key, token)
+        if card_movements:
+            for movement in card_movements:
+                if 'data' in movement and 'listAfter' in movement['data'] and movement['data']['listAfter']['id'] == column_id:
+                    movement_date = datetime.strptime(movement['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
+                    if from_date <= movement_date <= to_date:
+                        filtered_cards.append(card)
+                        break  # Uma vez que encontramos o movimento válido, podemos parar de verificar os outros movimentos
+    return filtered_cards
+
 # def filter_cards_by_column(cards, column_id,  api_key, token, from_date_str, to_date_str):
 #     # Convertendo as strings de data para objetos datetime
 #     from_date = datetime.strptime(from_date_str, '%Y-%m-%d')
 #     to_date = datetime.strptime(to_date_str, '%Y-%m-%d')
+#     # to_date = datetime.strptime(to_date_str + ' 23:59:59', '%Y-%m-%d %H:%M:%S')  # Adicionando o tempo para incluir o último segundo do dia
+
 
 #     filtered_cards = []
-#     # Adicionando tqdm para acompanhar o progresso
-#     for card in tqdm(cards, desc='Filtering cards'):
+
+#     def process_card(card):
 #         card_movements = get_card_movements(card['id'],api_key, token)
-#         # print(card_movements)
-#         for movement in card_movements:
-#             if 'listAfter' in movement and movement['listAfter']['id'] == column_id:
-#                 movement_date = datetime.strptime(movement['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
-#                 if from_date <= movement_date <= to_date:
-#                     filtered_cards.append(card)
-#                     break
-#     return filtered_cards
-
-def filter_cards_by_column(cards, column_id,  api_key, token, from_date_str, to_date_str):
-    # Convertendo as strings de data para objetos datetime
-    from_date = datetime.strptime(from_date_str, '%Y-%m-%d')
-    to_date = datetime.strptime(to_date_str, '%Y-%m-%d')
-    # to_date = datetime.strptime(to_date_str + ' 23:59:59', '%Y-%m-%d %H:%M:%S')  # Adicionando o tempo para incluir o último segundo do dia
-
-
-    filtered_cards = []
-
-    def process_card(card):
-        card_movements = get_card_movements(card['id'],api_key, token)
         
-        for movement in card_movements:
-            # print(f"{movement['data']['listAfter']['id']}")
-            # print(f"{movement['listAfter']['id']}")
-            # Verificando se o atributo 'listAfter' existe
-            if 'listAfter' in movement['data'] and movement['data']['listAfter']['id'] == column_id:
-                # print(f'{movement}')
-                movement_date = datetime.strptime(movement['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
-                # print(f"{from_date} <= {movement_date} <= {to_date}")
-                if from_date <= movement_date <= to_date:
-                    # print(f"[add card: {card['name']} - {movement_date}]")
-                    return card
-        return None
+#         for movement in card_movements:
+#             # print(f"{movement['data']['listAfter']['id']}")
+#             # print(f"{movement['listAfter']['id']}")
+#             # Verificando se o atributo 'listAfter' existe
+#             if 'listAfter' in movement['data'] and movement['data']['listAfter']['id'] == column_id:
+#                 # print(f'{movement}')
+#                 movement_date = datetime.strptime(movement['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
+#                 # print(f"{from_date} <= {movement_date} <= {to_date}")
+#                 if from_date <= movement_date <= to_date:
+#                     # print(f"[add card: {card['name']} - {movement_date}]")
+#                     return card
+#         return None
 
-    # Usando ThreadPoolExecutor para processar os cartões em paralelo
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(process_card, card) for card in cards]
+#     # Usando ThreadPoolExecutor para processar os cartões em paralelo
+#     with ThreadPoolExecutor() as executor:
+#         futures = [executor.submit(process_card, card) for card in cards]
 
-        # Adicionando tqdm para acompanhar o progresso
-        for future in tqdm(as_completed(futures), total=len(futures), desc='Filtering cards'):
-            filtered_card = future.result()
-            if filtered_card:
-                filtered_cards.append(filtered_card)
+#         # Adicionando tqdm para acompanhar o progresso
+#         for future in tqdm(as_completed(futures), total=len(futures), desc='Filtering cards'):
+#             filtered_card = future.result()
+#             if filtered_card:
+#                 filtered_cards.append(filtered_card)
 
-    return filtered_cards
+#     return filtered_cards
 
 def get_card_movements(card_id, api_key, token):
     """Obtém o histórico de movimentações de um cartão."""
@@ -154,17 +155,31 @@ def get_card_movements(card_id, api_key, token):
     response = requests.get(url, params=params)
     return response.json() if response.status_code == 200 else []
 
+# def memberInfoByCard(cards, from_date, to_date):
+#     # Coleta informações dos membros para cada card
+#     detailed_info = []
+#     with ThreadPoolExecutor() as executor:
+#         future_to_card = {executor.submit(process_card, card, API_KEY, TOKEN, from_date, to_date): card for card in cards}
+#         for future in tqdm(as_completed(future_to_card), total=len(future_to_card), desc='Processing cards'):
+#             card = future_to_card[future]
+#             try:
+#                 detailed_info.extend(future.result())
+#             except Exception as exc:
+#                 print(f'Error processing card {card}: {exc}')
+
+#     # Convertendo a lista de detalhes de cartões em um DataFrame
+#     df = pd.DataFrame(detailed_info)
+#     return df
+
 def memberInfoByCard(cards, from_date, to_date):
     # Coleta informações dos membros para cada card
     detailed_info = []
-    with ThreadPoolExecutor() as executor:
-        future_to_card = {executor.submit(process_card, card, API_KEY, TOKEN, from_date, to_date): card for card in cards}
-        for future in tqdm(as_completed(future_to_card), total=len(future_to_card), desc='Processing cards'):
-            card = future_to_card[future]
-            try:
-                detailed_info.extend(future.result())
-            except Exception as exc:
-                print(f'Error processing card {card}: {exc}')
+    for card in tqdm(cards, desc='Processing cards'):
+        try:
+            card_info = process_card(card, API_KEY, TOKEN, from_date, to_date)
+            detailed_info.extend(card_info)
+        except Exception as exc:
+            print(f'Error processing card {card}: {exc}')
 
     # Convertendo a lista de detalhes de cartões em um DataFrame
     df = pd.DataFrame(detailed_info)
@@ -198,10 +213,11 @@ def generateMetricsTrello(board_id, list_id, filename, from_date, to_date):
     # Filtrar os cartões pela movimentação para a coluna desejada (cards, column_name, from_date_str, to_date_str):
     filtered_cards = filter_cards_by_column(cards, list_id, API_KEY, TOKEN,from_date, to_date)
     df_filtered_cards = pd.DataFrame(filtered_cards)
-    # print(filtered_cards)
+    print(f"total de cards filtrados: {df_filtered_cards.shape[0]}")
     
     df = memberInfoByCard(filtered_cards, from_date, to_date)
-
+    print(f"total de info de membro do board em um card: {df.shape[0]}")
+    # print(df)
     # Coleta informações sobre membros não encontrados nos cards
     not_found_df = members_df[~members_df['fullName'].isin(df['Member Name'])]
 
